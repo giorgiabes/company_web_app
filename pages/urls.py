e73f671ce398d999ce6ad4d0415e82cb5f13bbc2
giorgiabes <abesadze.george@gmail.com>
2026-07-13 21:52:01 +0400

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+# pages/urls.py
+from django.urls import path
+
+from .views import home_page_view, AboutPageView
+
+urlpatterns = [
+    path("about/", AboutPageView.as_view()),
+    path("", home_page_view),
+]
